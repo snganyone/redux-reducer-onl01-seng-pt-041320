ffac0db3eab7a4a3fce5ed5cbb263aa479b8c662
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2021-01-12 16:48:47 -0800

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,9 +3,11 @@ let state = {friends: []};
 export function manageFriends(state, action){
     switch(action.type){
         case 'ADD_FRIEND': 
-            return {friends: state.friends.push(state.friends)}
+            return {...state, friends: [...state.friends, action.friend]}
         case 'REMOVE_FRIEND':
-            return {...state.friends}
+            let f = state.friends.find(f => f.id === action.id);
+
+            return {...state, friends: [...state.friends.slice(f)]}
         default:
             return state
     }
